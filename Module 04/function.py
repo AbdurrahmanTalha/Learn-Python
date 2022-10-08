a25b7693670474562cd475932f7cda173d093e8f
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-10-08 23:48:53 +0600

    Added Function.py

---
+++ Module 04/function.py
@@ -0,0 +1,21 @@
+def double_it(num):
+    result = num * 2
+    return result
+
+# double_it(34)
+
+
+def add(num1, num2):
+    total = num + num2
+    return total
+
+# add(56, 23)
+
+
+def multiply(num1, num2):
+    result = num1 * num2
+    return result
+
+
+output = multiply(25, 5)
+print(output)
