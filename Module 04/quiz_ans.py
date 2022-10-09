8fdd96c567d841a3ca4717cfbf9869a9bca069ce
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-10-09 20:22:49 +0600

    added list

---
+++ Module 04/quiz_ans.py
@@ -0,0 +1,6 @@
+def display(**kwargs):
+    for i in kwargs:
+        print(i, end=" ")
+
+
+display(num=1)
---
+++ Module 05/list.py
@@ -0,0 +1,7 @@
+nums = [12, 45, 65, 23, 89, 78, 11]
+
+# print(num[1])
+# print(nums[-6])
+# print(nums[1:5])
+
+print(nums[1:])
