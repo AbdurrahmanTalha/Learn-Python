c7d644120b4661b84d6243acbc8d6f5ef923779b
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-10-12 23:54:23 +0600

    finished video 3

--- Module 06/auto_wallpaper.py
+++ Module 06/auto_wallpaper.py
@@ -4,6 +4,7 @@
 # imports
 import requests
 import json
+import PyWallpaper
 
 # api url
 api_url = "https://api.nasa.gov/planetary/apod?api_key=mrXKluARVyKYD9kKRiaGcJecAg4uOnApwiHVJo7E"
@@ -22,3 +23,7 @@ res = requests.get(image_url)
 # save the image
 with open("apod.png", 'wb') as image:
     image.write(res.content)
+
+# set as wallpaper
+
+PyWallpaper.change_wallpaper("D:/project/Phitron/Python Course-1/Week-02/Module 06/apod.png")
