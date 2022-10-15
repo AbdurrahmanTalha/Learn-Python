6a10b9b0c1337d0fc0a0a4be798cecbb620a612e
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-10-15 21:00:51 +0600

    week 2 finished

---
+++ Module 05/build_in.py
@@ -0,0 +1,5 @@
+largest = max(45, 89, 54, 116, -12)
+nums = {12, 45, 65, 23, 89, 78, 11}
+
+big_nums = max(nums);
+print(largest, big_nums)
---
+++ Module 06.5/problem-3.py
@@ -0,0 +1,5 @@
+s = input("Enter your sentence: ");
+
+print(f"Lowercase: {s.lower()}")
+print(f"Uppercase: {s.upper()}")
+print(f"Capitalized: {s.capitalize()}")
