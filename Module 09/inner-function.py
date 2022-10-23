e252a6b6da400212ac31bd311b38b8f234f701b7
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-10-24 00:10:33 +0600

    Created week 04

--- Module 09/inner-function.py
+++
@@ -1,9 +0,0 @@
-def do_something():
-    print("inside of the function 'do_something'")
-
-    def inner_function():
-        print("Inside the inner function")
-    inner_function()
-
-
-do_something()
