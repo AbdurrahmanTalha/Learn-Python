c0f35753616a229f40eb05ca8793fbf4501dd24e
AbdurrahmanTalha <abdurrahmantalha331@gmail.com>
2022-10-25 03:48:14 +0600

    added school_management.py

--- Module 12/school_management.py
+++ Module 12/school_management.py
@@ -9,6 +9,14 @@ class School:
         new_grade = Grade(name, teacher)
         self.grades.append(new_grade)
 
+    def remove_grade(self, name):
+        idx = 0
+        for i, grade in enumerate(self.grades):
+            if grade.name == name:
+                idx = i
+
+        del self.grades[idx]
+
 
 class Grade:
     def __init__(self, name, teacher) -> None:
@@ -19,9 +27,15 @@ class Grade:
     def __repr__(self) -> str:
         return f"'{self.name}', '{self.teachers}'"
 
+    def __del__(self) -> None:
+        print(f"Deleting {self.name} with teacher {self.teachers}")
+
 
 oxford = School("Oxford kid Academy", "Mirpur", "Obayed Alam")
-oxford.add_grade("Class 3", "Osman Gani")
 oxford.add_grade("Class 1", "Nahim Bulhan")
+oxford.add_grade("Class 3", "Osman Gani")
+oxford.add_grade("Class 5", "Bulhan Udin")
+
+oxford.remove_grade("Class 5")
 
 print(oxford.grades)
